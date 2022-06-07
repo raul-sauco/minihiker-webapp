511c56414ecd5c964151699cca0959956a58f48a
Raul Sauco <sauco.raul@gmail.com>
2022-06-07 17:17:24 +0200

    Improve publishing logic

--- launch-dev-env.sh
+++ launch-dev-env.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# https://stackoverflow.com/a/5947802/2557030
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 # https://stackoverflow.com/a/48843074/2557030
 #Open Docker, only if is not running
 if (! docker stats --no-stream ); then
@@ -11,9 +15,10 @@ while (! docker stats --no-stream ); do
   echo "Waiting for Docker to launch..."
   sleep 1
 done
+fi
+# Will have no effect if the containers are up already
 docker compose up -d --remove-orphans
 sleep 5
-fi
 
 # Import the legacy schema used before migrations
 echo 'Importing legacy schema into development database...'
@@ -21,6 +26,14 @@ mysql --host 127.0.0.1 --port 34107 -umh_user -ppassword --database mhdb < data/
 
 echo 'Importing legacy schema into test database...'
 mysql --host 127.0.0.1 --port 34106 -umh_test_user -ppassword --database mh_test < data/latest.sql
+
+if [ -f composer.phar ]; then
+  echo 'Installing composer dependencies'
+  php composer.phar install
+else
+    echo -e "${YELLOW}» WARNING! Missing composer.phar file you need to install the dependencies manually${NC}"
+    echo "» Head over to https://getcomposer.org/download/ and follow the instructions there"
+fi
 
 echo 'Updating configuration files with development defaults'
 php init --env=Development --overwrite=All
--- publish.sh
+++ publish.sh
@@ -1,19 +1,77 @@
 #!/bin/bash
 
-# Login and backup the database
-# ssh raul@minihiker.com
-# mysqldump -u root -p mh > /home/raul/backups/mh_latest.sql
+# Store the destination in a variable
+REMOTE="raul@minihiker.com"
+DEST=raul@minihiker.com:app
 
-# Backup existing files
-# rsync -avzh raul@minihiker.com:minihiker /media/i/HDD/backups/
-# rsync -avzh raul@minihiker.com:backups/* /media/i/HDD/backups/mh/
+# Clean the dist directory
+rm static/css/*
+
+# Compile css from scss
+if ! command -v sass &> /dev/null
+then
+    echo "<sass> could not be found, installing"
+    npm install -g sass
+fi
+sass backend/sass/:static/css/
+
+# Minimize CSS
+if ! command -v cleancss &> /dev/null
+then
+    echo "<cleancss> could not be found, installing"
+    npm install clean-css-cli -g
+fi
+cleancss --batch --batch-suffix '.min' static/css/*.css
+
+# Minimize JS
+if ! command -v uglifyjs &> /dev/null
+then
+    echo "<uglifyjs> could not be found, installing"
+    npm install uglify-js -g
+fi
+rm static/js/*.min.js;
+for f in static/js/*.js; do
+  short=${f%.js};
+  uglifyjs "$f" > "$short".min.js;
+done
+
+# Login and backup the database
+echo
+echo "*** Backing up remote databases in host $REMOTE ***"
+echo
+ssh -T $REMOTE <<'EOL'
+	now="$(date)"
+	name="$HOSTNAME"
+	up="$(uptime)"
+	echo "Server name is $name"
+	echo "Server date and time is $now"
+	echo "Server uptime: $up"
+	cd scripts
+	./mysql_backup.sh
+EOL
+
+# Backup server database before update and migrations
+rsync -avzh $REMOTE:backups/db data/
 
 # Publish new work
-DEST=raul@minihiker.com:backend
+rsync -avzh --exclude-from './excludes.txt' . $DEST
 
-# Publish the Minihiker backend code
-# from workspace ==> to minihiker.com/backend
-rsync -avzh --exclude-from './excludes.txt' /home/i/ws/mh/app/ $DEST
+echo "Local system name: $HOSTNAME"
+echo "Local date and time: $(date)"
 
-# If file deletion is needed
-# rsync -avzh --delete from to
+# Install new dependencies and apply new migrations
+echo
+echo "*** Installing dependencies and applying migrations in host $REMOTE ***"
+echo
+ssh -T $REMOTE <<'EOL'
+	now="$(date)"
+	name="$HOSTNAME"
+	up="$(uptime)"
+	echo "Server name is $name"
+	echo "Server date and time is $now"
+	echo "Server uptime: $up"
+	cd app
+	php composer.phar install
+	./yii migrate --interactive=0
+	echo "Server is up to date"
+EOL
